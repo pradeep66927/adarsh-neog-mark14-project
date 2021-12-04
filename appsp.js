97088cb3d7065181b6e1064a6c4e1f680055a33e
pkthakur <pradeepthakur66927@gmail.com>
2021-12-05 00:50:50 +0530

    code that tell about your stocks profit or loss

---
+++ appsp.js
@@ -0,0 +1,45 @@
+var initialPrice = document.querySelector("#initial-price");
+
+var stocksQuantity = document.querySelector("#stocks-quantity");
+
+var currentPrice = document.querySelector("#Current-price");
+
+var submitBtn = document.querySelector("#submit-btn");
+
+var outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener("click",submitHandler);
+function submitHandler() {
+    var ip = Number(initialPrice.value);
+    var qty = Number(stocksQuantity.value);
+    var curr = Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip,qty,curr);
+}
+
+function calculateProfitAndLoss(initial,quantity,
+current) {
+    if (initial > current) {
+        var loss = (initial - current)*quantity;
+        var lossPercentage = (loss/initial)*100;
+
+        showOutput(`Hey, the loss is ${loss} and the 
+        percent is ${lossPercentage}%`
+        );
+        // loss logic here
+    } else if (current > initial) {
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit/initial)*100;
+
+        showOutput(`Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+        );
+        //profit logic here
+    }else {
+        showOutput("no pain no gain and no gain no pain");
+        //rest of code 
+    } 
+}
+
+function showOutput(message) {
+    outputBox.innerHTML = message
+}
